abe77e227a50756400cf6b4ad9b35876a5527194
danilo-justino <126601206+danilo-justino@users.noreply.github.com>
2026-01-04 10:17:25 -0300

    Remove unnecessary newline at end of silver_customers.sql

---
+++ SQL/DDL/SILVER/silver_customers.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS SILVER_CUSTOMERS
+(
+    customer_id STRING,
+    company_name STRING,
+    contact_name STRING,
+    contact_title STRING,
+    country STRING,
+    city STRING,
+    address STRING,
+    phone STRING,
+    postal_code STRING,
+    filename STRING,
+    created_at TIMESTAMP
+)
+;
