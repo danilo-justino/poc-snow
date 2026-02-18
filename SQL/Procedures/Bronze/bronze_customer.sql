5280aca6e049abcb0d9e2e4c678b6ccc3c84d6b9
danilo-justino <126601206+danilo-justino@users.noreply.github.com>
2026-02-18 16:07:22 -0300

    Create load_bronze_customers procedure
    This procedure loads data into the bronze_customers table from a specified location, truncating the table first.

---
+++ SQL/Procedures/Bronze/bronze_customer.sql
@@ -0,0 +1,37 @@
+-- Procedure Bronze_Customers
+
+ 
+
+CREATE OR REPLACE PROCEDURE load_bronze_customers()
+
+RETURNS STRING 
+
+LANGUAGE SQL
+
+AS
+
+$$
+
+BEGIN 
+
+    TRUNCATE TABLE bronze_customers;
+
+    INSERT INTO bronze_customers
+        SELECT 
+            CAST($1 AS VARIANT) as raw,
+            metadata$filename as filename,                     
+            CURRENT_TIMESTAMP() as created_at 
+    FROM @POC.PUBLIC.NORTH/customers   (FILE_FORMAT => PARQUET_FORMAT) ;
+ 
+
+    RETURN 'Tabela Bronze Customers carregada com sucesso';
+
+ 
+
+END;
+
+$$;
+
+
+
+truncate bronze_customers;
